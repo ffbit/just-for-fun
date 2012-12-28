6494f6f07d6bdb96705c0edc4e3405fb80ef682b
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-12-28 23:55:26 +0200

    The itShouldHaveExpectedEntryForTwo and itShouldHaveExpectedEntryForThree tests are green.

--- src/main/java/com/ffbit/fun/batch/BatchProcessor.java
+++ src/main/java/com/ffbit/fun/batch/BatchProcessor.java
@@ -1,13 +1,32 @@
 package com.ffbit.fun.batch;
 
 import java.util.Collections;
-import java.util.ListIterator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class BatchProcessor {
 
-    public Map<Integer, int[]> process(ListIterator<int[]> data) {
-        return Collections.singletonMap(1, new int[] {1, 11});
+    public Map<Integer, List<Integer>> process(List<int[]> tuples) {
+        if (tuples.isEmpty()) {
+            return Collections.<Integer, List<Integer>>emptyMap();
+        }
+
+        Map<Integer, List<Integer>> result = new HashMap<Integer, List<Integer>>();
+
+        for (int[] tuple : tuples) {
+            int key = tuple[0];
+            int value = tuple[1];
+
+            if (!result.containsKey(key)) {
+                result.put(key, new LinkedList<Integer>());
+            }
+
+            result.get(key).add(value);
+        }
+
+        return result;
     }
 
 }
--- src/test/java/com/ffbit/fun/batch/BatchProcessorTest.java
+++ src/test/java/com/ffbit/fun/batch/BatchProcessorTest.java
@@ -1,7 +1,7 @@
 package com.ffbit.fun.batch;
 
 import java.util.Arrays;
-import java.util.ListIterator;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Before;
@@ -12,27 +12,37 @@ import static org.junit.Assert.assertThat;
 
 public class BatchProcessorTest {
 
-    private ListIterator<int[]> data;
+    private List<int[]> tuples;
     private BatchProcessor batchProcessor;
-    private Map<Integer, int[]> result;
+    private Map<Integer, List<Integer>> result;
 
     @Before
     public void setUp() throws Exception {
         batchProcessor = new BatchProcessor();
-        data = Arrays.asList(
+        tuples = Arrays.asList(
                 new int[]{1, 1},
                 new int[]{1, 11},
                 new int[]{2, 2},
                 new int[]{2, 22},
                 new int[]{2, 222},
                 new int[]{3, 3}
-        ).listIterator();
-        result = batchProcessor.process(data);
+        );
+        result = batchProcessor.process(tuples);
     }
 
     @Test
     public void itShouldHaveExpectedEntryForOne() throws Exception {
-        assertThat(result, hasEntry(1, new int[]{1, 11}));
+        assertThat(result, hasEntry(1, Arrays.asList(1, 11)));
+    }
+
+    @Test
+    public void itShouldHaveExpectedEntryForTwo() throws Exception {
+        assertThat(result, hasEntry(2, Arrays.asList(2, 22, 222)));
+    }
+
+    @Test
+    public void itShouldHaveExpectedEntryForThree() throws Exception {
+        assertThat(result, hasEntry(3, Arrays.asList(3)));
     }
 
 }
