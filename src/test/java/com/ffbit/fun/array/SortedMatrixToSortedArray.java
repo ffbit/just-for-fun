9260f887ac70c248cb7635886d07d0c37677bc56
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-01-09 01:11:24 +0200

    The SortedMatrixToSortedArray has been solved.

--- src/test/java/com/ffbit/fun/array/SortedMatrixToSortedArray.java
+++ src/test/java/com/ffbit/fun/array/SortedMatrixToSortedArray.java
@@ -5,7 +5,53 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+
+/**
+ * <p>
+ * Solution for the <a href="http://www.careercup.com/question?id=15072838">Amazon Interview Question</a>
+ * </p>
+ * <pre>
+ * You have a two dimensional array of size m*n. The
+ * elements in the rows are sorted and every row has
+ * unique elements means( in a row no two elements are same) but
+ * the elements can repeat across the rows.
+ * For example:
+ * you have following 2-D array:
+ * {
+ * 2 5 7 9 14 16
+ * 3 6 8 10 15 21
+ * 4 7 9 15 22 35
+ * 7 8 9 22 40 58
+ * }
+ * You are supposed to write an efficient function which
+ * will take upper 2-D array as input and will return a
+ * one-dimensional array with following properties:
+ * a) the 1-D array must contain all the elements of above
+ * 2-D array.
+ * b) the 1-D array should not have any repeated elements.
+ * c) all the elements should be in sorted order.
+ * For example:
+ * for the above 2-D array, the output should be:
+ * A [ ] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 15, 16, 21, 22, 35, 40, 58 }
+ * Function Prototype should be :
+ * int [ ] MergeAndSort( int[ ][ ] inputArray )
+ * </pre>
+ */
 public class SortedMatrixToSortedArray {
+
+    @Test
+    public void testMergeAndSortTaskMatrix() throws Exception {
+        int[][] matrix = {
+                {2, 5, 7, 9, 14, 16},
+                {3, 6, 8, 10, 15, 21},
+                {4, 7, 9, 15, 22, 35},
+                {7, 8, 9, 22, 40, 58}
+        };
+        int[] expected = {2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 15, 16, 21, 22, 35,
+                40, 58};
+
+        assertThat(mergeAndSort(matrix), is(expected));
+    }
 
     @Test
     public void testMergeAndSortEmptyMatrix() throws Exception {
