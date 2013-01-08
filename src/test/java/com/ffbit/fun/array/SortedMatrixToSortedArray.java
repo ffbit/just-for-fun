43b654791d5a57a9b17c964bf8a36ac0ef75ab04
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-01-09 00:22:36 +0200

    testMergeTwoEmptySortedArrays and findDistinctLength tests are green.

--- src/test/java/com/ffbit/fun/array/SortedMatrixToSortedArray.java
+++ src/test/java/com/ffbit/fun/array/SortedMatrixToSortedArray.java
@@ -15,7 +15,49 @@ public class SortedMatrixToSortedArray {
     }
 
     private int[] mergeAndSort(int[][] matrix) {
-        return new int[] {};
+        return new int[]{};
+    }
+
+    @Test
+    public void testMergeTwoEmptySortedArrays() throws Exception {
+        int[] x = {};
+        int[] y = {};
+        int[] expected = {};
+
+        assertThat(mergeAndSort(x, y), is(expected));
+    }
+
+    private int[] mergeAndSort(int[] x, int[] y) {
+        return new int[]{};
+    }
+
+    @Test
+    public void testFindDistinctLength() throws Exception {
+        int[] x = {0, 1};
+        int[] y = {1, 2, 3};
+
+        assertThat(findDistinctLength(x, y), is(4));
+    }
+
+    private int findDistinctLength(int[] x, int[] y) {
+        int i = 0;
+        int j = 0;
+        int n = 0;
+
+        while (i < x.length && j < y.length) {
+            if (x[i] < y[j]) {
+                i++;
+            } else if (x[i] > y[j]) {
+                j++;
+            } else {
+                i++;
+                j++;
+            }
+
+            n++;
+        }
+
+        return n + x.length - i + y.length - j;
     }
 
 }
