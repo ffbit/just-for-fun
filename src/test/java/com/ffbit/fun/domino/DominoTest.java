ec551387610b5800cfe6d3df6c54f05a3a832787
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-02-19 16:17:03 +0200

    Dominoes. Code clean up yet again.

--- src/test/java/com/ffbit/fun/domino/DominoTest.java
+++ src/test/java/com/ffbit/fun/domino/DominoTest.java
@@ -1,39 +1,38 @@
 package com.ffbit.fun.domino;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-import org.junit.Test;
-
 public class DominoTest {
 
     @Test
     public void testDomino() throws Exception {
-        int n = 12;
-        int size = n + 1;
+        int maxFace = 12;
+        int size = maxFace + 1;
         int count = 15;
-        List<int[]> dominoes = generateDominoes(n, count);
+        List<int[]> dominoes = generateDominoes(maxFace, count);
 
-        System.out.println("Dominoes: "
-                + Arrays.deepToString(dominoes.toArray()));
+        System.out.println("Dominoes: " + Arrays.deepToString(dominoes.toArray()));
 
-        int[][] matrix = new int[size][size];
+        int[][] graph = new int[size][size];
         boolean[] faces = new boolean[size];
 
         for (int[] domino : dominoes) {
             int i = domino[0];
             int j = domino[1];
 
-            matrix[i][j] = 1;
-            matrix[j][i] = 1;
+            graph[i][j] = 1;
+            graph[j][i] = 1;
 
             faces[i] = true;
             faces[j] = true;
         }
 
-        printMatrix(matrix);
+        printGraph(graph);
 
         int[] emptyTrain = new int[count + 1 + 1];
         int[] maxTrain = emptyTrain.clone();
@@ -44,7 +43,7 @@ public class DominoTest {
                 int[] train = emptyTrain.clone();
                 train[0] = i;
 
-                train = backtrack(matrix, i, train);
+                train = buildTrain(graph, i, train);
 
                 if (maxTrain[trainLengthIndex] < train[trainLengthIndex]) {
                     maxTrain = train;
@@ -57,36 +56,36 @@ public class DominoTest {
         }
 
         int maxLength = maxTrain[trainLengthIndex];
-        System.out.print("the longest train of length " + maxLength + ": ");
+        System.out.print("the longest train is of length " + maxLength + ": ");
         for (int i = 0; i < maxLength; i++) {
             System.out.print("[" + maxTrain[i] + ":" + maxTrain[i + 1] + "]");
         }
     }
 
-    private int[] backtrack(int[][] matrix, int start, int[] train) {
+    private int[] buildTrain(int[][] graph, int start, int[] train) {
         int trainLengthIndex = train.length - 1;
         int trainLength = train[trainLengthIndex];
-        int[] row = matrix[start];
+        int[] row = graph[start];
 
         int[] localMaxTrain = train;
 
-        for (int i = 0; i < row.length && trainLength < train.length; i++) {
+        for (int i = 0; i < row.length; i++) {
             if (row[i] != 0) {
                 int[] localTrain = train.clone();
                 int localTrainLength = trainLength + 1;
                 localTrain[trainLengthIndex] = localTrainLength;
                 localTrain[localTrainLength] = i;
-                matrix[start][i] = 0;
-                matrix[i][start] = 0;
+                graph[start][i] = 0;
+                graph[i][start] = 0;
 
-                localTrain = backtrack(matrix, i, localTrain);
+                localTrain = buildTrain(graph, i, localTrain);
 
                 if (localMaxTrain[trainLengthIndex] < localTrain[trainLengthIndex]) {
                     localMaxTrain = localTrain;
                 }
 
-                matrix[start][i] = 1;
-                matrix[i][start] = 1;
+                graph[start][i] = 1;
+                graph[i][start] = 1;
             }
         }
         return localMaxTrain;
@@ -97,22 +96,22 @@ public class DominoTest {
                 + train[train.length - 1]);
     }
 
-    private void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
+    private void printGraph(int[][] graph) {
+        for (int[] row : graph) {
             System.out.println(Arrays.toString(row));
         }
     }
 
-    private List<int[]> generateDominoes(int size, int count) {
-        assert size > 0;
+    private List<int[]> generateDominoes(int maxFace, int count) {
+        assert maxFace > 0;
         assert count > 0;
-        assert count <= (size + 1) * size / 2;
+        assert count <= (maxFace + 1) * maxFace / 2;
 
         List<int[]> deck = new ArrayList<int[]>();
 
-        for (int i = 0; i <= size; i++) {
-            for (int j = i; j <= size; j++) {
-                deck.add(new int[] { i, j });
+        for (int i = 0; i <= maxFace; i++) {
+            for (int j = i; j <= maxFace; j++) {
+                deck.add(new int[]{i, j});
             }
         }
 
