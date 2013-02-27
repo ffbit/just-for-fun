7cf8c9ba609a29c97e562a4c0dce6973b4b1cd2b
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-02-27 17:48:13 +0200

    Swap Two Integers without the third one: the difference way. Red

---
+++ src/test/java/com/ffbit/fun/SwapTwoIntegersWithoutTheThirdOneTest.java
@@ -0,0 +1,22 @@
+package com.ffbit.fun;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SwapTwoIntegersWithoutTheThirdOneTest {
+
+    @Test
+    public void testDifferenceWay() throws Exception {
+        int x = 2;
+        int y = 3;
+
+        // algorithm's idea goes here
+        // ...
+
+        assertThat(x, is(3));
+        assertThat(y, is(2));
+    }
+
+}
