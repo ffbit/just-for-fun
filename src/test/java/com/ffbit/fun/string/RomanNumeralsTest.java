02a8b12d3b2aabb30405454ff84097e5e8bfbe25
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-02-28 10:33:11 +0200

    RomanNumeralsTest: moved switch to a separate method.

--- src/test/java/com/ffbit/fun/string/RomanNumeralsTest.java
+++ src/test/java/com/ffbit/fun/string/RomanNumeralsTest.java
@@ -1,15 +1,15 @@
 package com.ffbit.fun.string;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(Parameterized.class)
 public class RomanNumeralsTest {
@@ -23,26 +23,26 @@ public class RomanNumeralsTest {
 
     @Parameters
     public static Collection<Object[]> init() {
-        return Arrays.asList(new Object[][] {
-                { "I", 1 },
-                { "V", 5 },
-                { "X", 10 },
-                { "L", 50 },
-                { "C", 100 },
-                { "D", 500 },
-                { "M", 1000 },
+        return Arrays.asList(new Object[][]{
+                {"I", 1},
+                {"V", 5},
+                {"X", 10},
+                {"L", 50},
+                {"C", 100},
+                {"D", 500},
+                {"M", 1000},
 
-                { "II", 2 },
-                { "VI", 6 },
+                {"II", 2},
+                {"VI", 6},
 
-                { "IV", 4 },
-                { "MMXIII", 2013 },
-                { "DCCCXLI", 841 }
+                {"IV", 4},
+                {"MMXIII", 2013},
+                {"DCCCXLI", 841}
         });
     }
 
     @Test
-    public void testConvertionRomanNumberToDecimal() throws Exception {
+    public void testConversionRomanNumberToDecimal() throws Exception {
         assertThat(romanToInt(romanNumber), is(exptectedDecimalNumber));
     }
 
@@ -53,34 +53,33 @@ public class RomanNumeralsTest {
         for (char digit : digits) {
             int currentNumber = 0;
 
-            switch (digit) {
-            case 'I':
-                currentNumber = 1;
-                break;
-            case 'V':
-                currentNumber = 5;
-                break;
-            case 'X':
-                currentNumber = 10;
-                break;
-            case 'L':
-                currentNumber = 50;
-                break;
-            case 'C':
-                currentNumber = 100;
-                break;
-            case 'D':
-                currentNumber = 500;
-                break;
-            case 'M':
-                currentNumber = 1000;
-                break;
-            }
+            currentNumber = romanDigitToDecimalNumber(digit);
 
             result += currentNumber;
         }
 
         return result;
+    }
+
+    private int romanDigitToDecimalNumber(char digit) {
+        switch (digit) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
     }
 
 }
