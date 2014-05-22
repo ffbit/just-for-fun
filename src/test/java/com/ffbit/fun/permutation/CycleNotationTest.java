c078098f8e7e824252fd597f3c039c14ee23c1b8
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-05-23 00:14:42 +0300

    Cycle Notation Test - Introduced Generics.

--- src/test/java/com/ffbit/fun/permutation/CycleNotationTest.java
+++ src/test/java/com/ffbit/fun/permutation/CycleNotationTest.java
@@ -25,30 +25,30 @@ public class CycleNotationTest {
 
     @Test
     public void cyclePermutation() throws Exception {
-        int[] origin =      {1, 2, 3, 4, 5, 6, 7, 8};
-        int[] permutation = {4, 5, 7, 6, 8, 2, 1, 3};
+        Integer[] origin =      {1, 2, 3, 4, 5, 6, 7, 8};
+        Integer[] permutation = {4, 5, 7, 6, 8, 2, 1, 3};
 
         assertThat(toCycleNotation(origin, permutation), is("(1 4 6 2 5 8 3 7)"));
     }
 
     @Test
     public void multipleCyclePermutation() throws Exception {
-        int[] origin =      {1, 2, 3, 4, 5, 6, 7, 8};
-        int[] permutation = {4, 5, 7, 6, 2, 8, 1, 3};
+        Integer[] origin =      {1, 2, 3, 4, 5, 6, 7, 8};
+        Integer[] permutation = {4, 5, 7, 6, 2, 8, 1, 3};
 
         assertThat(toCycleNotation(origin, permutation), is("(1 4 6 8 3 7)(2 5)"));
     }
 
-    private String toCycleNotation(int[] origin, int[] permutation) {
-        Map<Integer, Integer> mapping = new LinkedHashMap<Integer, Integer>(origin.length, 1);
+    private <T> String toCycleNotation(T[] origin, T[] permutation) {
+        Map<T, Integer> mapping = new LinkedHashMap<T, Integer>(origin.length, 1);
         for (int i = 0; i < origin.length; i++) {
             mapping.put(origin[i], i);
         }
 
-        List<Set<Integer>> cycles = new ArrayList<Set<Integer>>();
-        Set<Integer> cycle = new LinkedHashSet<Integer>();
+        List<Set<T>> cycles = new ArrayList<Set<T>>();
+        Set<T> cycle = new LinkedHashSet<T>();
 
-        Integer key = null;
+        T key = null;
         Integer index = 0;
         while (!mapping.isEmpty()) {
             if (key == null) {
@@ -56,7 +56,7 @@ public class CycleNotationTest {
                     cycles.add(cycle);
                 }
 
-                cycle = new LinkedHashSet<Integer>();
+                cycle = new LinkedHashSet<T>();
                 key = mapping.keySet().iterator().next();
                 cycle.add(key);
                 index = mapping.remove(key);
@@ -81,7 +81,7 @@ public class CycleNotationTest {
         assertThat(toCycles(Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5))), is("(1 2 3)(4 5)"));
     }
 
-    private <C extends Collection<Integer>> String toCycles(Collection<C> collections) {
+    private <T, C extends Collection<T>> String toCycles(Collection<C> collections) {
         StringBuilder cycles = new StringBuilder();
 
         for (C c : collections) {
@@ -96,11 +96,11 @@ public class CycleNotationTest {
         assertThat(toCycle(Arrays.asList(1, 2, 3)), is("(1 2 3)"));
     }
 
-    private String toCycle(Collection<Integer> collection) {
+    private <T> String toCycle(Collection<T> collection) {
         StringBuilder cycle = new StringBuilder("(");
         String separator = "";
 
-        for (Integer e : collection) {
+        for (T e : collection) {
             cycle.append(separator)
                     .append(e);
             separator = " ";
@@ -110,4 +110,5 @@ public class CycleNotationTest {
 
         return cycle.toString();
     }
+
 }
