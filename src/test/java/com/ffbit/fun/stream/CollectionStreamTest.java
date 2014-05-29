264343bcd6d230b25ac27ae23da0e06e9ed1ac76
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-05-29 19:34:13 +0300

    Java 8 Streams
    Counting even numbers.

--- src/test/java/com/ffbit/fun/stream/CollectionStreamTest.java
+++ src/test/java/com/ffbit/fun/stream/CollectionStreamTest.java
@@ -16,8 +16,13 @@ public class CollectionStreamTest {
     private List<Integer> list = asList(1, 2, 3, 4, 5, 1);
 
     @Test
-    public void itShouldFilterEven() throws Exception {
+    public void itShouldFilterEvenNumbers() throws Exception {
         assertThat(list.stream().filter(e -> e % 2 == 0).collect(toList()), is(asList(2, 4)));
+    }
+
+    @Test
+    public void itShouldCountEvenNumbers() throws Exception {
+        assertThat(list.stream().filter(e -> e % 2 == 0).count(), is(2L));
     }
 
 }
