d6d38cf1a5c02dce5b558d5a329cae2a0d1b635d
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-09-26 23:03:10 +0300

    Fixed the MergeBottomUpSort

--- src/main/java/com/ffbit/fun/sorting/MergeBottomUpSort.java
+++ src/main/java/com/ffbit/fun/sorting/MergeBottomUpSort.java
@@ -7,35 +7,37 @@ public class MergeBottomUpSort implements Sort {
     @Override
     public int[] sort(int[] source) {
         int[] auxiliary = Arrays.copyOf(source, source.length);
-        mergesort(source, 0, source.length, auxiliary);
+        mergesort(source, new int[source.length]);
 
         return source;
     }
 
-    private void mergesort(int[] source, int lo, int hi, int[] auxiliary) {
-        if (hi - lo == 1) {
-            return;
+    private void mergesort(int[] source, int[] auxiliary) {
+        int N = source.length;
+        for (int n = 1; n < N; n += n) {
+            for (int i = 0; i < N - n; i += 2 * n) {
+                int lo = i;
+                int middle = lo + n - 1;
+                int hi = Math.min(i + 2 * n, N) - 1;
+                merge(source, lo, middle, hi, auxiliary);
+            }
         }
-
-        int middle = lo + (hi - lo) / 2;
-        mergesort(source, lo, middle, auxiliary);
-        mergesort(source, middle + 1, hi, auxiliary);
-        System.arraycopy(source, lo, auxiliary, lo, hi - lo);
-        merge(source, lo, middle, hi, auxiliary);
     }
 
     private void merge(int[] source, int lo, int middle, int hi, int[] auxiliary) {
+        System.arraycopy(source, lo, auxiliary, lo, hi - lo + 1);
+
         int i = lo;
-        int j = middle;
-        for (int k = lo; k < hi; k++) {
-            if (i == middle) {
-                source[k] = auxiliary[++j];
-            } else if (j == hi) {
+        int j = middle + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > middle) {
+                source[k] = auxiliary[j++];
+            } else if (j > hi) {
                 source[k] = auxiliary[i++];
             } else if (auxiliary[i] <= auxiliary[j]) {
                 source[k] = auxiliary[i++];
             } else {
-                source[k] = auxiliary[++j];
+                source[k] = auxiliary[j++];
             }
         }
     }
