c97c94dfbf7920b3b5ed5ac0e2689d95c38d7850
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2015-01-24 21:14:37 +0200

    Longest Common Subsequence one subsequence finder.

--- src/main/java/com/ffbit/fun/array/LongestCommonSubSequence.java
+++ src/main/java/com/ffbit/fun/array/LongestCommonSubSequence.java
@@ -5,23 +5,88 @@ import java.util.Arrays;
 public class LongestCommonSubSequence {
 
     public int findCommonSubSequenceSize(char[] a, char[] b) {
-        int[][] matrix = new int[a.length + 1][b.length + 1];
+        int[][] commonMatrix = buildCommonMatrix(a, b);
 
-        for (int i = 1; i <= a.length; i++) {
-            for (int j = 1; j <= b.length; j++) {
+        return commonMatrix[a.length][b.length];
+    }
+
+    public char[] findCommonSubSequence(char[] a, char[] b) {
+        int[][] commonMatrix = buildCommonMatrix(a, b);
+
+        int commonLength = commonMatrix[a.length][b.length];
+        char[] commonSubSequence = new char[commonLength];
+
+
+        int i = a.length;
+        int j = b.length;
+        while (commonLength > 0) {
+            int current = commonMatrix[i][j];
+            int left = commonMatrix[i][j - 1];
+            int diagonal = commonMatrix[i - 1][j - 1];
+            int up = commonMatrix[i - 1][j];
+
+            // get increased
+            // turn diagonal
+            if (current > diagonal
+                    && diagonal == left
+                    && diagonal == up) {
+                commonSubSequence[commonLength - 1] = b[i - 1];
+                commonLength--;
+                i--;
+                j--;
+            }
+            // turn left
+            else if (current == left) {
+                j--;
+            }
+            // turn up
+            else if (current == up) {
+                i--;
+            }
+
+        }
+
+        return commonSubSequence;
+    }
+
+    private int[][] buildCommonMatrix(char[] a, char[] b) {
+        int[][] commonMatrix = new int[b.length + 1][a.length + 1];
+
+        for (int i = 1; i <= b.length; i++) {
+            for (int j = 1; j <= a.length; j++) {
                 int increase = 0;
-                if (a[i - 1] == b[j - 1]) {
+                if (b[i - 1] == a[j - 1]) {
                     increase = 1;
                 }
 
-                matrix[i][j] = Math.max(
-                        Math.max(matrix[i - 1][j - 1], matrix[i - 1][j]),
-                        matrix[i][j - 1]
+                commonMatrix[i][j] = Math.max(
+                        Math.max(
+                                commonMatrix[i - 1][j - 1],
+                                commonMatrix[i - 1][j]
+                        ),
+                        commonMatrix[i][j - 1]
                 ) + increase;
             }
         }
 
-        return matrix[a.length][b.length];
+        logMatrix(a, b, commonMatrix);
+
+        return commonMatrix;
+    }
+
+    private void logMatrix(char[] a, char[] b, int[][] commonMatrix) {
+        System.out.println();
+        System.out.print("[0]   ");
+        System.out.println(Arrays.toString(a));
+        for (int i = 0; i < commonMatrix.length; i++) {
+            if (i == 0) {
+                System.out.print("   ");
+            } else {
+                System.out.print("[" + b[i - 1] + "]");
+            }
+            System.out.println(Arrays.toString(commonMatrix[i]));
+        }
+        System.out.println();
     }
 
 }
--- src/test/java/com/ffbit/fun/array/LongestCommonSubSequenceTest.java
+++ src/test/java/com/ffbit/fun/array/LongestCommonSubSequenceTest.java
@@ -9,7 +9,7 @@ public class LongestCommonSubSequenceTest {
     private LongestCommonSubSequence finder = new LongestCommonSubSequence();
 
     @Test
-    public void itShouldFindLongestCommonSubSequence2() {
+    public void itShouldFindLongestCommonSubSequenceSize2() {
         char[] a = "abcd".toCharArray();
         char[] b = "badc".toCharArray();
 
@@ -17,11 +17,30 @@ public class LongestCommonSubSequenceTest {
     }
 
     @Test
-    public void itShouldFindLongestCommonSubSequence5() {
+    public void itShouldFindLongestCommonSubSequenceSize5() {
         char[] a = "abcdefg".toCharArray();
         char[] b = "abgcfde".toCharArray();
 
         assertThat(finder.findCommonSubSequenceSize(a, b), is(5));
+    }
+
+
+    @Test
+    public void itShouldFindLongestCommonSubSequence2() {
+        char[] a = "abcd".toCharArray();
+        char[] b = "badc".toCharArray();
+        char[] commonSubSequence = "ac".toCharArray();
+
+        assertThat(finder.findCommonSubSequence(a, b), is(commonSubSequence));
+    }
+
+    @Test
+    public void itShouldFindLongestCommonSubSequence5() {
+        char[] a = "abcdefg".toCharArray();
+        char[] b = "abgcfde".toCharArray();
+        char[] commonSubSequence = "abcde".toCharArray();
+
+        assertThat(finder.findCommonSubSequence(a, b), is(commonSubSequence));
     }
 
 }
