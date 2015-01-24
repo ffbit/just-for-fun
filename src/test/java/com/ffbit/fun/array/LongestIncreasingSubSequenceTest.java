0030aebea99fbd0075d27ba01d4ce76f547d91c6
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2015-01-24 12:14:44 +0200

    Found a controversial failing example for the Longest Increasing Subsequence.

--- src/test/java/com/ffbit/fun/array/LongestIncreasingSubSequenceTest.java
+++ src/test/java/com/ffbit/fun/array/LongestIncreasingSubSequenceTest.java
@@ -10,10 +10,17 @@ public class LongestIncreasingSubSequenceTest {
             = new LongestIncreasingSubSequence();
 
     @Test
-    public void itShouldFindLongestIncreasingSubSequence() {
+    public void itShouldFindLongestIncreasingSubSequenceOf3() {
         int[] sequence = {7, 2, 6, 3, 7, 4, 1};
 
         assertThat(subSequenceFinder.find(sequence), is(new int[]{2, 3, 4}));
+    }
+
+    @Test // Fails - a controversial example was found
+    public void itShouldFindLongestIncreasingSubSequenceOf4() {
+        int[] sequence = {7, 2, 6, 3, 7, 4, 1, 5};
+
+        assertThat(subSequenceFinder.find(sequence), is(new int[]{2, 3, 4, 5}));
     }
 
 }
